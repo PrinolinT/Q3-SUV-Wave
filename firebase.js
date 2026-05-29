cf50953a71d0ddade29a8048d2daf55d1107ff18
PrinolinT <prinolin.thaver@dunlop.co.za>
2026-05-29 16:21:59 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,37 @@
+// firebase.js
+
+import { initializeApp } from "firebase/app";
+
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  serverTimestamp
+} from "firebase/firestore";
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDg0vFZwgUnb1Cjer7w-wb95_YjG8MDw54",
+
+  authDomain: "q3-suv-wave.firebaseapp.com",
+
+  projectId: "q3-suv-wave",
+
+  storageBucket: "q3-suv-wave.firebasestorage.app",
+
+  messagingSenderId: "785277358729",
+
+  appId: "1:785277358729:web:e326909768b69606452c58"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export {
+  db,
+  collection,
+  addDoc,
+  serverTimestamp
+};
