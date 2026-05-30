ec8dc68bd01004b24a414bf20706c64cc967a883
PrinolinT <prinolin.thaver@dunlop.co.za>
2026-05-30 10:09:58 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,36 @@
+import {
+  db,
+  collection,
+  doc,
+  getDoc
+}
+from "./firebase.js";
 
+async function loadDashboard() {
+
+  try {
+
+    // PARTICIPANTS
+
+    const participantsSnapshot =
+      await getDocs(
+        collection(
+          db,
+          "participants"
+        )
+      );
+
+    document.getElementById(
+      "participantsCount"
+    ).innerText =
+      participantsSnapshot.size;
+
+  } catch (error) {
+
+    console.error(error);
+
+  }
+
+}
+
+loadDashboard();
